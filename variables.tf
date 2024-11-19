2aaf9b8960e0ce7ab2ab3b0d17f422b25c10f629
Jensen Kokou <104699414+jensenkokou@users.noreply.github.com>
2024-11-18 23:42:12 -0600

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "git_branch" {
+  type = string
+}
+
+variable "git_commit_sha" {
+  type = string
+}
+
+variable "coder_url" {
+  type = string
+}
+
+variable "coder_ci_token" {
+  type = string
+}
